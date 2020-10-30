135e5b41b0a0fd5d65e0205cb8e50425e89b3e61
nawazprince <nawazprince120@gmail.com>
2020-10-30 14:28:21 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $(".sidebar-btn").click(function(e){
+        e.preventDefault();
+        $(".sidebar").toggleClass("collapse");
+    });
+
+  $('.menu-btn').click(function(e) {
+    e.preventDefault();
+    $(this).siblings('.sub-menu').slideToggle();
+    $(this).find('.drop-down').toggleClass("rotate");
+});
+
+$('.drop-down-btn').click(function (e) { 
+  e.preventDefault();
+  $(this).siblings('.drop-down-links').slideToggle();
+  $(this).find('.drop-down').toggleClass("rotate");
+});
+})
